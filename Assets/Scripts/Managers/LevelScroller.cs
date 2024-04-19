41f7cc411d1baaac8b1b7ba494a429240e2d1b83
SÉBASTIEN\sebgu <sebastien.guarober@gmail.com>
2024-04-19 12:51:18 +0200

    Made level endless + Adds a starting tile

--- Assets/Scripts/Managers/LevelScroller.cs
+++ Assets/Scripts/Managers/LevelScroller.cs
@@ -7,43 +7,24 @@ using static UnityEngine.AdaptivePerformance.Provider.AdaptivePerformanceSubsyst
 public class LevelScroller : MonoBehaviour
 {
     [SerializeField] SplineManager splineManager;
-    [SerializeField] OldObstacleManager OldObstacleManager;
     [SerializeField] ObstacleManager ObstacleManager;
 
     [SerializeField] float ScrollSpeed;
 
     List<float> TempFloats = new List<float>();
 
-    private void Start()
-    {
-        for(int i = 0; i < OldObstacleManager.obstacleInfos.Count; i++)
-        {
-            TempFloats.Insert(i, OldObstacleManager.obstacleInfos[i].locationOnSpline);
-        }
-    }
-
     private void Update()
     {
-        for(int i = 0; i < ObstacleManager.InstantiatedTiles.Count; ++i)
+        if(ObstacleManager.LeadingTileObject != null)
+        {
+            GameObject obj = ObstacleManager.LeadingTileObject;
+            obj.transform.position += ((-obj.transform.forward) * ScrollSpeed) * Time.deltaTime;
+        }
+
+        for (int i = 0; i < ObstacleManager.InstantiatedTiles.Count; ++i)
         {
             GameObject obj = ObstacleManager.InstantiatedTiles[i];
             obj.transform.position += ((-obj.transform.forward) * ScrollSpeed) * Time.deltaTime;
-        }
-        for(int i = 0; i < OldObstacleManager.obstacleInfos.Count; ++i)
-        {
-            ObstacleInfo info = OldObstacleManager.obstacleInfos[i];
-           /* GameObject obj = OldObstacleManager.instantiatedGOs[i];
-            obj.transform.position += ((-obj.transform.forward) * ScrollSpeed) * Time.deltaTime;
-            if (info.movesAlongSpline)
-            {
-                TempFloats[i] = Mathf.Repeat(TempFloats[i] + (info.moveAlongSplineSpeed * Time.deltaTime), 1.0f);
-
-                Vector3 PosAlongSpline = new Vector3(splineManager.PlayerSpline.PlayerSpline.EvaluatePosition(TempFloats[i]).x,
-                    splineManager.PlayerSpline.PlayerSpline.EvaluatePosition(TempFloats[i]).y,
-                    obj.transform.position.z);
-
-                obj.transform.position = PosAlongSpline;
-            }*/
         }
     }
 }
