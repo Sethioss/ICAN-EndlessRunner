78247b6bd5da9e7f4a6aa5c591ac2a6bff20822f
SÉBASTIEN\sebgu <sebastien.guarober@gmail.com>
2024-05-24 18:47:13 +0200

    Added base of side jump (Currently unfinished)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] OnSplineMovementController MovementController;
     [SerializeField] LevelScroller LevelScroller;
 
     [SerializeField] MeshRenderer PlayerMesh;
